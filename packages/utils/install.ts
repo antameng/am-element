9853bc23381c520d66ea997b84ef3ccc6cd1458c
aimeng <671608311@qq.com>
2024-05-05 23:50:44 +0800

    格式化ts

--- packages/utils/install.ts
+++ packages/utils/install.ts
@@ -1,16 +1,16 @@
-import  type { App,Plugins } from 'vue'
-import {each} from 'lodash-es'
+import type { App, Plugin } from 'vue'
+import { each } from 'lodash-es'
 
-type SFCWithInstall<T>=T &  Plugins
+type SFCWithInstall<T> = T & Plugin
 
-export  function makeInstaller(component:Plugins[]){
-    const installer=(app:App)=>each(component,c=>app.use(c));
-    return installer;
+export function makeInstaller(component: Plugin[]) {
+    const installer = (app: App) => each(component, c => app.use(c));
+    return installer as Plugin;
 }
-export  const  withInstaller = <T>(component:T)=>{
-    (component as SFCWithInstall<T>).install=(app:App)=>{
-        const name =(component as any)?.name||"UnnamedComponent";
-        app.component(name,component as SFCWithInstall<T>)
+export const withInstaller = <T>(component: T) => {
+    (component as SFCWithInstall<T>).install = (app: App) => {
+        const name = (component as any)?.name || "UnnamedComponent";
+        app.component(name, component as Plugin)
     }
     return component as SFCWithInstall<T>;
 }
